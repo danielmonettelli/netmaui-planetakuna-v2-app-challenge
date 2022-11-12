4cbc2dbb3877acf8204c84e913ce99f91a7048b4
Daniel Monettelli <danielmonetelli@hotmail.com>
2022-11-12 16:50:10 -0500

    🚀 [Code] Added "UseMauiCommunityToolkit( )" method to "MauiProgram" class

--- Planetakuna/MauiProgram.cs
+++ Planetakuna/MauiProgram.cs
@@ -1,24 +1,20 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace Planetakuna;
-
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("customfonticons.ttf", "CustomFontIcons");
-				fonts.AddFont("Kanit-Light.ttf", "Kanit#300");
-			});
-
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("customfonticons.ttf", "CustomFontIcons");
+            fonts.AddFont("Kanit-Light.ttf", "Kanit#300");
+        }).UseMauiCommunityToolkit();
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
-
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
